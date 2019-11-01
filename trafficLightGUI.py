5c371adf48e82f2bc9fb4f0860a739b9d1fc2652
TakezoCan <cj.hapkido@gmail.com>
2019-11-01 07:35:08 -0600

    GUIlights

--- trafficLightGUI.py
+++ trafficLightGUI.py
@@ -9,50 +9,53 @@
 
 from guizero import App, PushButton, Text  # GUI interface app
 from time import sleep # import sleep function for delay timing
-#import RPi.GPIO as GPIO # import GPIO library to use GPIO pins
+import RPi.GPIO as GPIO # import GPIO library to use GPIO pins
 
 # Lables for BCM pin numbers
-#greenLed = 18
-#yellowLed = 22
-#redLed = 25
-#state = False
+greenLed = 18
+yellowLed = 22
+redLed = 25
+state = False
 
 #pushNum = 4
 
-#GPIO.setwarnings(False) # stop warnings from displaying
-#GPIO.setmode(GPIO.BCM) # BCM numbering scheme for breakout board
-#GPIO.setup(greenLed,GPIO.OUT) # sets variable greenLed to an output
-#GPIO.setup(yellowLed,GPIO.OUT) # sets variable yellowLed to an output
-#GPIO.setup(redLed,GPIO.OUT) # sets variable redLed to an output
+GPIO.setwarnings(False) # stop warnings from displaying
+GPIO.setmode(GPIO.BCM) # BCM numbering scheme for breakout board
+GPIO.setup(greenLed,GPIO.OUT) # sets variable greenLed to an output
+GPIO.setup(yellowLed,GPIO.OUT) # sets variable yellowLed to an output
+GPIO.setup(redLed,GPIO.OUT) # sets variable redLed to an output
 #GPIO.setup(pushNum,GPIO.IN, pull_up_down=GPIO.PUD_UP) # sets variable pushNum to an input
 
 def counter():
     text.value = int(text.value) + 1
 
 def run_lights(): #opperates outputs
-    #if (start_button.enable == False):
     print("Running Lights")
-    #GPIO.output(redLed,GPIO.LOW)    # Turns off redLed
-    #GPIO.output(yellowLed,GPIO.LOW) # Turns off yellowLed
-    #GPIO.output(greenLed,GPIO.HIGH) # Turns on greenLed
-    #sleep(3.0)  # Wait 3 seconds
+    GPIO.output(redLed,GPIO.LOW)    # Turns off redLed
+    GPIO.output(yellowLed,GPIO.LOW) # Turns off yellowLed
+    GPIO.output(greenLed,GPIO.HIGH) # Turns on greenLed
+    sleep(3.0)  # Wait 3 seconds
 
-    #GPIO.output(greenLed,GPIO.LOW)  # Turns off greenLed
-    #GPIO.output(redLed,GPIO.LOW)    # Turns off redLed
-    #GPIO.output(yellowLed,GPIO.HIGH)# Turns on yellowLed
-    #sleep(3.0)  # Wait 3 seconds
+    GPIO.output(greenLed,GPIO.LOW)  # Turns off greenLed
+    GPIO.output(redLed,GPIO.LOW)    # Turns off redLed
+    GPIO.output(yellowLed,GPIO.HIGH)# Turns on yellowLed
+    sleep(3.0)  # Wait 3 seconds
 
-    #GPIO.output(yellowLed,GPIO.LOW) # Turns off yellowLed
-    #GPIO.output(greenLed,GPIO.LOW)  # Turns off greenLed
-    #GPIO.output(redLed,GPIO.HIGH)   # Turns on redLed
-    #sleep(3.0)  # Wait 3 seconds
+    GPIO.output(yellowLed,GPIO.LOW) # Turns off yellowLed
+    GPIO.output(greenLed,GPIO.LOW)  # Turns off greenLed
+    GPIO.output(redLed,GPIO.HIGH)   # Turns on redLed
+    sleep(3.0)  # Wait 3 seconds
 
+def run_cleanUP():
+    GPIO.output(redLed,GPIO.LOW)    # Turns off redLed
+    GPIO.output(yellowLed,GPIO.LOW) # Turns off yellowLed
+    GPIO.output(greenLed,GPIO.LOW) # Turns off greenLed
 
 def start():
     start_button.disable()
     stop_button.enable()
-    start_button.repeat(100,run_lights)
-    message.value = "Traffic lights ON"
+    start_button.repeat(100,run_lights) # Repeat run_lights loop every 100ms
+    message.value = "Traffic lights ON" # Displays message in GUI
     #message.color = "green"
     print("Traffic lights ON")
 
@@ -60,13 +63,15 @@ def start():
 def stop():
     start_button.enable()
     stop_button.disable()
-    stop_button.cancel(run_lights)
+    stop_button.cancel(run_lights) # Cancels run_lights loop
     #GPIO.cleanup() # Clean up GPIO pins
+    run_cleanUP()
     message.value = "Traffic lights OFF"
     #message.color = "red"
     print("Traffic lights OFF")
 
-app = App(title="Trafic Lights",width=300, height=200, layout="auto")
+#Start of GUI Loop
+app = App(title="Trafic Lights",width=300, height=200, layout="auto") # Sets up GUI window
 
 print("Traffic lights OFF")
 message = Text(app, text="Traffic lights OFF", size=20, font="Times New Roman", color="Black")
@@ -80,6 +85,6 @@ text = Text(app, text="1")
 text.repeat(1000, counter)  # Schedule call to counter() every 1000ms
 
 
-app.display()
-
+app.display() # Similar to While loop. Runs from "app=..." to "app.display"
+#End of GUI loop
 
